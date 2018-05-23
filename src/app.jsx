8d579366a01eb442f9ce109130c80cefac8e9f6b
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-05-23 17:24:22 +0200

    setting up files

---
+++ src/app.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Sass from "./scss/main.scss";
+
+const promise = new Promise((resolve, reject) => {});
+
+class Test extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    fetchData = () => {
+      fetch("https://api.github.com/users/MateuszKowalskiCL").then(resp => {
+
+      })
+    };
+
+    render () {
+        return <div>
+            <p>element jest stabilny</p>
+            <button onClick = {() => this.fetchData()}>fetch data</button>
+        </div>
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(
+        <Test/>,
+        document.getElementById("root")
+    )
+});
